f757d0920706db1900480a08480c8df4f1fd7c69
Zihad Malik <zihadm654@gmail.com>
2022-03-03 10:26:59 +0600

    removed env variable

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DB_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGE_SENDER,
-  appId: process.env.APP_ID,
+  apiKey: "AIzaSyAITQLz7ewSKRzzBI9yZo8bExX2qi8oYUU",
+  authDomain: "portfolio-next-50c1e.firebaseapp.com",
+  databaseURL: "https://portfolio-next-50c1e-default-rtdb.firebaseio.com",
+  projectId: "portfolio-next-50c1e",
+  storageBucket: "portfolio-next-50c1e.appspot.com",
+  messagingSenderId: "18054685625",
+  appId: "1:18054685625:web:5f277c169487736bca4c68",
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
