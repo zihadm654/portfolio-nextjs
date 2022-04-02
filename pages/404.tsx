e0cba2085433b196a8c689dfe7ba60a26855914c
Zihad Malik <zihadm654@gmail.com>
2022-04-02 11:40:51 +0600

    variable name updated

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ const Index = () => {
   return (
     <div className="wrong">
       <h2 className="description">Awwww...this page is not available</h2>
-      <Button text="Back to Home" className="primary__btn" site="/" />
+      <Button text="Back to Home" variants="primary__btn" site="/" />
     </div>
   );
 };
