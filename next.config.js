df2cf753ce3773a958580acbd1cd7fa655342487
Abdul Malek <zihadm654@gmail.com>
2024-02-18 11:14:26 +0600

    pwa configured with service worker & others

--- next.config.js
+++ next.config.js
@@ -1,33 +1,19 @@
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants');
+const { withContentlayer } = require('next-contentlayer');
+const path = require('path');
 /**
  * @type{import("next").NextConfig}
  *
  */
-// import {
-//   PHASE_DEVELOPMENT_SERVER,
-//   PHASE_PRODUCTION_BUILD,
-// } from 'next/constants.js';
-const { withContentlayer } = require('next-contentlayer');
-// import { withContentlayer } from 'next-contentlayer';
-const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
-  // experimental: {
-  //   serverActions: true,
-  //   // serverComponentsExternalPackages: ['@prisma/client'], // <-- and this
-  // },
-  // serverActions,
-  // and the following to enable top-level await support for Webpack
-  // webpack: (config) => {
-  //   config.experiments = {
-  //     ...config.experiments,
-  //     topLevelAwait: true,
-  //   };
-  //   return config;
-  // },
-  // sassOptions: {
-  //   includePaths: [path.join(__dirname, 'styles')],
-  // },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   images: {
     remotePatterns: [
       {
@@ -36,35 +22,33 @@ const nextConfig = {
         // port: '',
         // pathName: '/dxadtnltj/image/upload/**',
       },
-      // 'storage.googleapis.com',
-      // 'firebasestorage.googleapis.com',
-      // 'res.cloudinary.com',
     ],
   },
 };
 
-// const nextConfigFunction = async (phase) => {
-//   if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-//     const withPWA = (await import('@ducanh2912/next-pwa')).default({
-//       dest: 'public',
-//       disable: process.env.NODE_ENV === 'development',
-//       register: true,
-//       scope: '/app',
-//       sw: 'service-worker.js',
-//       customWorkerDir: 'serviceworker',
-//       fallbacks: {
-//         document: '/~offline',
-//         data: '/fallback.json',
-//         image: '/fallback.webp',
-//         font: '/fallback-font.woff2',
-//       },
-//     });
-//     return withPWA(nextConfig);
-//   }
-//   return withContentlayer(nextConfig);
-// };
-
-// module.exports = withContentlayer(nextConfig);
-
-// export default withContentlayer(nextConfig);
-module.exports = withContentlayer(nextConfig);
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = require('@ducanh2912/next-pwa').default({
+      dest: 'public',
+      disable: process.env.NODE_ENV === 'development',
+      register: true,
+      cacheOnFrontEndNav: true,
+      reloadOnOnline: true,
+      swcMinify: true,
+      workboxOptions: {
+        disableDevLogs: true,
+      },
+      scope: '/app',
+      sw: 'service-worker.js',
+      customWorkerDir: 'serviceworker',
+      fallbacks: {
+        document: '/~offline',
+        data: '/fallback.json',
+        image: '/fallback.webp',
+        font: '/fallback-font.woff2',
+      },
+    });
+    return withPWA(nextConfig);
+  }
+  return withContentlayer(nextConfig);
+};
